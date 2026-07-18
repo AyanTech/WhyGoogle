d514c4d2f093fd57ef921d2f842a0a9303224bed
Farshid <Farshid.roohi.a@Gmail.com>
2026-07-18 15:54:06 +0330

    Refactor: Bump Kotlin and gradle version, Fix build issues

---
+++ whygoogle/build.gradle.kts
@@ -0,0 +1,48 @@
+import org.gradle.api.publish.maven.MavenPublication
+
+plugins {
+    alias(libs.plugins.android.library)
+    id("maven-publish")
+}
+android {
+    namespace = "ir.ayantech.whygoogle"
+    compileSdk {
+        version = release(36) {
+            minorApiLevel = 1
+        }
+    }
+
+    defaultConfig {
+        minSdk = 21
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    testOptions {
+        unitTests.isReturnDefaultValues = true
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
+
+}
+
+dependencies {
+    implementation(libs.kotlin.stdlib)
+    implementation(libs.kotlin.reflect)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    api(libs.nonfinalviewpager2)
+    api(libs.gson)
+}
--- whygoogle/src/main/java/ir/ayantech/whygoogle/widget/RtlSwipeBackContainer.kt
+++ whygoogle/src/main/java/ir/ayantech/whygoogle/widget/RtlSwipeBackContainer.kt
@@ -131,19 +131,19 @@ class RtlSwipeBackContainer(context: Context, attrs: AttributeSet? = null) :
             previousValue = currentValue
         }
         animator.addListener(object : Animator.AnimatorListener {
-            override fun onAnimationStart(animation: Animator?) {
+            override fun onAnimationStart(animation: Animator) {
                 beginFakeDrag()
             }
 
-            override fun onAnimationEnd(animation: Animator?) {
+            override fun onAnimationEnd(animation: Animator) {
                 endFakeDrag()
                 onFragmentCreationEndedCallback?.invoke()
             }
 
-            override fun onAnimationCancel(animation: Animator?) { /* Ignored */
+            override fun onAnimationCancel(animation: Animator) { /* Ignored */
             }
 
-            override fun onAnimationRepeat(animation: Animator?) { /* Ignored */
+            override fun onAnimationRepeat(animation: Animator) { /* Ignored */
             }
         })
         animator.interpolator = interpolator
